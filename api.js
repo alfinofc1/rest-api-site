7425cd2a78a0a177d4f3270e1e0c76298eb87d4c
alfinofc1 <160604519+alfinofc1@users.noreply.github.com>
2025-02-19 23:19:55 +0700

    Add files via upload

--- api.js
+++ api.js
@@ -202,23 +202,26 @@ router.get("/downloader/fb", (req, res) => {
         });
 });
 ///====================textpro=============
-router.get('/downloader/randomquote', (req, res, next) => {
+router.get("/gpt4", (req, res) => {
+    const text = req.query.q || req.query.query;
 
-    fetch(encodeURI(`https://mhankbarbar.tech/api/randomquotes`))
-        .then(response => response.json())
-        .then(data => {
-             res.json({
-                 status : true,
-                 creator : 'alfinofc',
-                 result : {
-                     author : `${data.author}`,
-                     quotes : `${data.quotes}`,
-                 },
-                 message : 'selamat menggunakan'
-             })
-         })
-         .catch(e => {})
-})
+    // هنا تحط variable ل text,
+    // يعني بدل تكتب الجملة ذي كل مرة req.query.q || req.query.query 
+    //تكتب text وفقط
+
+    if(!text) return res.send({status: false, owner: '@إسمك', err: 'اكتب شيئا !'});
+  // if(!text) تعني في حالة ما كتب شي ترسل له السطر ذا الي تقول فيه اكتب شي
+
+    axios.get('https://bk9.fun/ai/chataibot?q='+text)
+        .then((response) => {
+            const responseData = response.data; //هنا يأخذ المعلومات من الرابط فوق ويحطهم عندك
+            res.send({status: true, owner: '@إسمك', BK9: responseData});
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send({status: false, owner: '@إسمك', err: 'الخادم مشغول الآن. حاول مرة أخرى في وقت لاحق!'});
+        });
+});
 //================================================= A I =================================================
 router.get("/ai", (req, res) => {
     fs.readFile("./html/ai.html", 'utf8', (err, data) => {
